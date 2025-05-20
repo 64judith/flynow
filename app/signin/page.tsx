0e4d7aaa3d7cb2633d5df51d9e117b81dca50afc
64judith <judithukadike7@gmail.com>
2025-05-20 08:49:32 +0100

    initialization

---
+++ app/signin/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="signin">
+      <div className="signin__box">
+        <div className="signin__box__similar signin__box__slide"></div>
+        <div className="signin__box__similar signin__box__info"></div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
